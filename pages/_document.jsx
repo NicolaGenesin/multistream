8b48ccd34153ee3b226531cbbff391b5de5e052b
Nicola Genesin <nicolagenesin@yahoo.it>
2021-03-11 00:50:39 +0100

    Add Google Analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -13,20 +13,21 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>Multistream.gg</title>
-          <link rel="icon" href="/favicon.ico" />
-          <meta name="description" content="Find and watch multiple Twitch streams on the same screen." />
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-H9391JK7MX"
+          />
 
-          <meta property="og:url" content="https://multistream.gg/" />
-          <meta property="og:type" content="website" />
-          <meta property="og:title" content="Multistream" />
-          <meta property="og:description" content="Find and watch multiple Twitch streams on the same screen." />
-
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta property="twitter:domain" content="multistream.gg" />
-          <meta property="twitter:url" content="https://multistream.gg/" />
-          <meta name="twitter:title" content="Multistream" />
-          <meta name="twitter:description" content="Find and watch multiple Twitch streams on the same screen." />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H9391JK7MX');
+            `,
+            }}
+          />
         </Head>
         <body>
           <Main />
