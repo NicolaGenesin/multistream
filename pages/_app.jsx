98c2f751f8d732da52719f63afc54b247bc5d72e
Nicola Genesin <nicolagenesin@yahoo.it>
2021-03-10 20:07:10 +0100

    Update Head information

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -13,7 +13,22 @@ const theme = extendTheme({
 const App = ({ Component, pageProps }) => (
   <ChakraProvider theme={theme}>
     <Fonts />
-    <Head />
+    <Head>
+      <title>Multistream.gg</title>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="description" content="Find and watch multiple Twitch streams on the same screen." />
+
+      <meta property="og:url" content="https://multistream.gg/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Multistream" />
+      <meta property="og:description" content="Find and watch multiple Twitch streams on the same screen." />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="multistream.gg" />
+      <meta property="twitter:url" content="https://multistream.gg/" />
+      <meta name="twitter:title" content="Multistream" />
+      <meta name="twitter:description" content="Find and watch multiple Twitch streams on the same screen." />
+    </Head>
     <Component {...pageProps} />
     <style global jsx>
       {`
