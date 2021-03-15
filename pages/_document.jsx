c94a13781a0b9f860544d8042de6eb0edb1bc5ad
Nicola Genesin <nicolagenesin@yahoo.it>
2021-03-15 15:23:41 +0100

    Revert "GA autotrack test (trying to track shallow url changes)"
    This reverts commit 4aa5dc0abf2bf3a47c1f1a1199283958c35a2155.

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -17,24 +17,14 @@ class MyDocument extends Document {
             async
             src="https://www.googletagmanager.com/gtag/js?id=G-H9391JK7MX"
           />
-          <script async src="https://www.google-analytics.com/analytics.js" />
-          <script async src="https://cdnjs.cloudflare.com/ajax/libs/autotrack/2.4.1/autotrack.js" />
+
           <script
             dangerouslySetInnerHTML={{
               __html: `
-              window.ga=window.ga||function()
-              {(ga.q = ga.q || []).push(arguments)}
-              ;ga.l=+new Date;
-              ga('create', 'G-H9391JK7MX', 'auto');
-              ga('require', 'eventTracker');
-              ga('require', 'outboundLinkTracker');
-              ga('require', 'urlChangeTracker');
-              ga('send', 'pageview');
-
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-H9391JK7MX');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H9391JK7MX');
             `,
             }}
           />
