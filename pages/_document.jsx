a3f5e2bb6424706aec869ab99d3b63fb23cf4ac1
Nicola Genesin <NicolaGenesin@users.noreply.github.com>
2021-11-26 01:20:11 +0100

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,7 @@
-import * as React from "react";
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import * as React from 'react';
+import Document, {
+  Html, Head, Main, NextScript,
+} from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -16,10 +18,7 @@ class MyDocument extends Document {
             src="https://www.googletagmanager.com/gtag/js?id=G-H9391JK7MX"
           />
           <script async src="https://www.google-analytics.com/analytics.js" />
-          <script
-            async
-            src="https://cdnjs.cloudflare.com/ajax/libs/autotrack/2.4.1/autotrack.js"
-          />
+          <script async src="https://cdnjs.cloudflare.com/ajax/libs/autotrack/2.4.1/autotrack.js" />
           <script
             dangerouslySetInnerHTML={{
               __html: `
@@ -40,7 +39,6 @@ class MyDocument extends Document {
             }}
           />
         </Head>
-        )
         <body>
           <Main />
           <NextScript />
